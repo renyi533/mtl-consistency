e884b1d5f565c452b7def2f3b7388b84cd5bb559
henrybjren <henrybjren@tencent.com>
2023-01-16 22:47:59 +0800

    tune early stop

--- Ali-CCP/run_example.sh
+++ Ali-CCP/run_example.sh
@@ -16,10 +16,10 @@ while [ $i -lt $iter ]
 do
   for model in mmoe ple
   do
-    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_dml_$i --mode training --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention True --global_experience True --epoch 10 > logs/${model}_dml_train_$i.log 2>&1 &
-    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_$i --mode training --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention False --global_experience False --epoch 10 > logs/${model}_train_$i.log 2>&1 &
-    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_attn_$i --mode training --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention True --global_experience False --epoch 10 > logs/${model}_attn_train_$i.log 2>&1 &
-    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_global_$i --mode training --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention False --global_experience True --epoch 10 > logs/${model}_global_train_$i.log 2>&1 &
+    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_dml_$i --mode training --early_stop 3 --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention True --global_experience True --epoch 50 > logs/${model}_dml_train_$i.log 2>&1 &
+    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_$i --mode training --early_stop 3 --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention False --global_experience False --epoch 50 > logs/${model}_train_$i.log 2>&1 &
+    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_attn_$i --mode training --early_stop 3 --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention True --global_experience False --epoch 50 > logs/${model}_attn_train_$i.log 2>&1 &
+    python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_global_$i --mode training --early_stop 3 --keep_prob 0.9,0.7,0.7  --model ${model} --co_attention False --global_experience True --epoch 50 > logs/${model}_global_train_$i.log 2>&1 &
     wait_function
     
     python ./model/train.py --model_dir ./ckpt/ali-ccp/${model}_dml_$i/best --mode pred --model ${model} --co_attention True --global_experience True --epoch 1 --pred_file logs/${model}_dml_result_$i.txt > logs/${model}_dml_test_$i.log 2>&1 &
@@ -29,7 +29,7 @@ do
     wait_function
   done  
     
-    python ./model/train.py --model_dir ./ckpt/ali-ccp/aitm_$i --mode training --keep_prob 0.9,0.7,0.7  --model aitm --epoch 10 > logs/aitm_train_$i.log 2>&1 &
+    python ./model/train.py --model_dir ./ckpt/ali-ccp/aitm_$i --mode training --early_stop 3 --keep_prob 0.9,0.7,0.7  --model aitm --epoch 50 > logs/aitm_train_$i.log 2>&1 &
     wait_function
     python ./model/train.py --model_dir ./ckpt/ali-ccp/aitm_$i/best --mode pred --model aitm --epoch 1 --pred_file logs/aitm_dml_result_$i.txt > logs/aitm_test_$i.log 2>&1 &
     wait_function
